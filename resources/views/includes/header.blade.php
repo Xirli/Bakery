4a3205ef07f1641f72ffd06ab151fbe60ca516df
UlyanaPyrohovska <pirozok2013>
2021-05-04 21:40:30 +0300

    header without layout menu

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,62 @@
+<div class="container" style="height: 122px"></div>
+<div class="stick_header">
+    <div class="header_wrap">
+        <div class="container header_wrap1">
+            <div class="row small_header">
+                <div class="col-2 c1">
+                    <div class="telephone">
+                        <img src="images/phone.svg" style="height: 20px; width: 20px"><span>+1-2222-44445-44</span>
+                    </div>
+                </div>
+                <div class="col-3 c1">
+                    <div class="email">
+                        <img src="images/email.svg" style="height: 20px; width: 20px"><span>bakery@gmail.com</span>
+                    </div>
+                </div>
+                <div class="col-4 c1">
+                    <div class="time-table">
+                        <img src="images/clock.svg" style="height: 20px; width: 20px"><span> Пн - Пт / 10:00-21:00, Сб - Нд / 09:00 - 23:00</span>
+                    </div>
+                </div>
+                <div class="col-2 c1 offset-1">
+                    <div class="social">
+                        <img src="images/instagram.svg" style="height: 20px; width: 20px">
+                        <img src="images/tiktok.svg" style="height: 20px; width: 20px">
+                        <img src="images/pinterest.svg" style="height: 20px; width: 20px">
+                        <img src="images/facebook.svg" style="height: 20px; width: 20px">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="menu_c">
+        <div class="container menu">
+            <div class="wrap_submenu">
+                <div class="submenu">
+                    <img class="menubar" src="images/menubar.svg"  alt="" tabindex="0">
+                    <img class="phone" src="images/phone_mob.svg">
+                    <a class="menu_a1">Меню</a>
+                    <a class="menu_a1">Клієнтам</a>
+                    <a class="menu_a1">Новини</a>
+                </div>
+            </div>
+            <div class="bakery">
+                <a>Bakery</a>
+            </div>
+            <div class="wrap_menu_icons" align="right">
+                <div class="menu_icons">
+                    <img src="images/user.svg">
+                    <img src="images/shopping_cart.svg">
+                </div>
+            </div>
+        </div>
+    </div>
+            <ul class="layout_submenu">
+                <a href=""><li class="head-text2">Хліб класичний</li></a>
+                <a href=""><li class="head-text2">Здобна випічка</li></a>
+                <a href=""><li class="head-text2">Пироги</li></a>
+                <a href=""><li class="head-text2">Веганська випічка</li></a>
+            </ul>
+
+
+</div>
