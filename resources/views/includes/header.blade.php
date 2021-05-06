79d1b54c9f5219ca699573db19ce03084f4a9bf8
UlyanaPyrohovska <pirozok2013>
2021-05-06 22:31:15 +0300

    header with layout menu

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,25 +5,25 @@
             <div class="row small_header">
                 <div class="col-2 c1">
                     <div class="telephone">
-                        <img src="images/phone.svg" style="height: 20px; width: 20px"><span>+1-2222-44445-44</span>
+                        <img src="public/images/phone.svg" style="height: 20px; width: 20px"><span>+1-2222-44445-44</span>
                     </div>
                 </div>
                 <div class="col-3 c1">
                     <div class="email">
-                        <img src="images/email.svg" style="height: 20px; width: 20px"><span>bakery@gmail.com</span>
+                        <img src="public/images/email.svg" style="height: 20px; width: 20px"><span>bakery@gmail.com</span>
                     </div>
                 </div>
                 <div class="col-4 c1">
                     <div class="time-table">
-                        <img src="images/clock.svg" style="height: 20px; width: 20px"><span> Пн - Пт / 10:00-21:00, Сб - Нд / 09:00 - 23:00</span>
+                        <img src="public/images/clock.svg" style="height: 20px; width: 20px"><span> Пн - Пт / 10:00-21:00, Сб - Нд / 09:00 - 23:00</span>
                     </div>
                 </div>
                 <div class="col-2 c1 offset-1">
                     <div class="social">
-                        <img src="images/instagram.svg" style="height: 20px; width: 20px">
-                        <img src="images/tiktok.svg" style="height: 20px; width: 20px">
-                        <img src="images/pinterest.svg" style="height: 20px; width: 20px">
-                        <img src="images/facebook.svg" style="height: 20px; width: 20px">
+                        <img src="public/images/instagram.svg" style="height: 20px; width: 20px">
+                        <img src="public/images/tiktok.svg" style="height: 20px; width: 20px">
+                        <img src="public/images/pinterest.svg" style="height: 20px; width: 20px">
+                        <img src="public/images/facebook.svg" style="height: 20px; width: 20px">
                     </div>
                 </div>
             </div>
@@ -33,11 +33,55 @@
         <div class="container menu">
             <div class="wrap_submenu">
                 <div class="submenu">
-                    <img class="menubar" src="images/menubar.svg"  alt="" tabindex="0">
-                    <img class="phone" src="images/phone_mob.svg">
-                    <a class="menu_a1">Меню</a>
-                    <a class="menu_a1">Клієнтам</a>
-                    <a class="menu_a1">Новини</a>
+                    <div class="dropdown">
+                        <button class="dropbtn">
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <ul class="dropdown-content">
+                            <li><a href="#">Меню</a>
+                                <ul>
+                                    <li><a href="#">Хліб класичний</a></li
+                                    ><li><a href="#">Здобна випічка</a></li
+                                    ><li><a href="#">Пироги</a></li
+                                    ><li><a href="#">Веганська випічка</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Клієнтам</a>
+                                <ul>
+                                    <li><a href="#">Про нас</a></li>
+                                    <li><a href="#">Контакти</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Новини</a></li>
+                        </ul>
+                    </div>
+                    <img class="phone" src="public/images/phone_mob.svg">
+                    <div class="dropdown1">
+                        <button class="dropbtn1">
+                            <a class="menu_a1">Меню</a>
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <ul class="dropdown-content1">
+                            <li><a href="#">Хліб класичний</a></li>
+                            <li><a href="#">Здобна випічка</a></li>
+                            <li><a href="#">Пироги</a></li>
+                            <li><a href="#">Веганська випічка</a></li>
+                        </ul>
+                    </div>
+                    <div class="dropdown1">
+                        <button class="dropbtn1">
+                            <a class="menu_a1">Клієнтам</a>
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <ul class="dropdown-content1">
+                            <li><a href="#">Про нас</a></li>
+                            <li><a href="#">Контакти</a></li>
+                        </ul>
+                    </div>
+                    <button class="dropbtn1">
+                        <a class="menu_a1">Новини</a>
+                        <i class="fa fa-caret-down"></i>
+                    </button>
                 </div>
             </div>
             <div class="bakery">
@@ -45,18 +89,10 @@
             </div>
             <div class="wrap_menu_icons" align="right">
                 <div class="menu_icons">
-                    <img src="images/user.svg">
-                    <img src="images/shopping_cart.svg">
+                    <img src="public/images/user.svg">
+                    <img src="public/images/shopping_cart.svg">
                 </div>
             </div>
         </div>
     </div>
-            <ul class="layout_submenu">
-                <a href=""><li class="head-text2">Хліб класичний</li></a>
-                <a href=""><li class="head-text2">Здобна випічка</li></a>
-                <a href=""><li class="head-text2">Пироги</li></a>
-                <a href=""><li class="head-text2">Веганська випічка</li></a>
-            </ul>
-
-
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset ('css/app.css') }}" rel="stylesheet"/>
-        <link href="{{ asset ('css/header.css') }}" rel="stylesheet"/>
+        <link href="{{ asset ('public/css/app.css') }}" rel="stylesheet"/>
+        <link href="{{ asset ('public/css/header.css') }}" rel="stylesheet"/>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=ABeeZee&display=swap" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet" />
